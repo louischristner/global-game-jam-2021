cce01ed2a2a970e56f2fdcee1911b4c47ee31e11
Louis CHRISTNER <louis.christner@epitech.eu>
2021-01-31 16:37:14 +0100

    Add scenes & exit

---
+++ global-game-jam-2021/Assets/Scripts/ExitManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitManager : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Salut");
+        if (other.gameObject.tag == "Player")
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ global-game-jam-2021/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
